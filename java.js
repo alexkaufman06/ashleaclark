5c0610e52234ac3707ed9494b0fad8e3a3c6b4e3
alexkaufman06 <mmstudent@CAMAH217-10.local>
2014-05-31 11:52:28 -0700

    Ashlea Clark's Portfolio
    This is my first website and is a portfolio for my friend Ashlea.

---
+++ java.js
@@ -0,0 +1,15 @@
+$(function() {
+    
+    window.onload=function() {
+        $("h1").fadeIn(5000);
+    };
+    
+    
+    //$("h1").click(function() {
+    //    alert("Hi! I'm Ashlea!");
+    //}); 
+ 
+    //$("document").onload(function(){
+    //    $("h1").fadeOut(5000);
+    //});
+});
